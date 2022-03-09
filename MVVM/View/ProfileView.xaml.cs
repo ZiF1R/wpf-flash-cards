d7df619d95cd0c51bfc96a47a2b0b88b80421108
ZiF1R <do-alex03@mail.ru>
2022-03-09 22:58:59 +0300

    Add icons for menu buttons

--- MVVM/View/ProfileView.xaml.cs
+++ MVVM/View/ProfileView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using course_project1.views;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace course_project1
+namespace course_project1.MVVM.View
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для ProfileView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ProfileView : UserControl
     {
-        public MainWindow()
+        public ProfileView()
         {
             InitializeComponent();
         }
---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course_project1.MVVM.ViewModel
+{
+    public class MainViewModel: ObservableObject
+    {
+        public ProfileViewModel ProfileVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            ProfileVM = new ProfileViewModel();
+            CurrentView = ProfileVM;
+        }
+    }
+}
