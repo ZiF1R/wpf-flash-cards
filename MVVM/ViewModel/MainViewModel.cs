7ff4b93f83761e121eb6a46b8cb267855a0d19fd
ZiF1R <do-alex03@mail.ru>
2022-03-10 16:49:11 +0300

    Add main pages to project

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,13 @@ namespace course_project1.MVVM.ViewModel
 {
     public class MainViewModel: ObservableObject
     {
+        public RelayCommand ProfileViewCommand { get; set; }
+        public RelayCommand FoldersViewCommand { get; set; }
+        public RelayCommand SettingsViewCommand { get; set; }
+
         public ProfileViewModel ProfileVM { get; set; }
+        public FoldersViewModel FoldersVM { get; set; }
+        public SettingsViewModel SettingsVM { get; set; }
         private object _currentView;
 
         public object CurrentView
@@ -26,7 +33,13 @@ namespace course_project1.MVVM.ViewModel
         public MainViewModel()
         {
             ProfileVM = new ProfileViewModel();
+            FoldersVM = new FoldersViewModel();
+            SettingsVM = new SettingsViewModel();
             CurrentView = ProfileVM;
+
+            ProfileViewCommand = new RelayCommand(() => CurrentView = ProfileVM);
+            FoldersViewCommand = new RelayCommand(() => CurrentView = FoldersVM);
+            SettingsViewCommand = new RelayCommand(() => CurrentView = SettingsVM);
         }
     }
 }
