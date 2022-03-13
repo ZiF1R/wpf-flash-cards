2a1fe00a748478779ba1c8506f687e6b9f028c5c
ZiF1R <do-alex03@mail.ru>
2022-03-13 19:26:20 +0300

    Finish app menu

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,12 @@ namespace course_project1
             InitializeComponent();
             //App app = new App();
             //app.Resources.MergedDictionaries
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
