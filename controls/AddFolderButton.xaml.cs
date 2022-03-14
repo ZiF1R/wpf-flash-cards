650ba264b0869d90ce70596c6fe3e7dc3df70510
ZiF1R <do-alex03@mail.ru>
2022-03-14 22:06:17 +0300

    Create AddFolderButton

---
+++ controls/AddFolderButton.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace course_project1.controls
+{
+    /// <summary>
+    /// Логика взаимодействия для AddFolderButton.xaml
+    /// </summary>
+    public partial class AddFolderButton : UserControl
+    {
+        public AddFolderButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
