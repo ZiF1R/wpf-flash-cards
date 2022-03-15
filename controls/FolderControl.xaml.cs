a81bb6163e889e15c004ea2b54f1ab6d60493b89
ZiF1R <do-alex03@mail.ru>
2022-03-15 22:36:21 +0300

    Add custom DependencyProperty for FolderControl

--- controls/FolderControl.xaml.cs
+++ controls/FolderControl.xaml.cs
@@ -24,5 +24,85 @@ namespace course_project1.controls
         {
             InitializeComponent();
         }
+
+        // Folder name
+        public static readonly DependencyProperty FolderNameProperty =
+            DependencyProperty.Register(
+               name: "FolderName",
+               propertyType: typeof(string),
+               ownerType: typeof(FolderControl),
+               typeMetadata: new FrameworkPropertyMetadata(
+                   defaultValue: "Default name",
+                   flags: FrameworkPropertyMetadataOptions.AffectsMeasure),
+               validateValueCallback: new ValidateValueCallback(IsValidString));
+
+        public string FolderName
+        {
+            get => (string)GetValue(FolderNameProperty);
+            set => SetValue(FolderNameProperty, value);
+        }
+
+        public static bool IsValidString(object value)
+        {
+            string val = (string)value;
+            return val != "";
+        }
+
+        // folder category
+        public static readonly DependencyProperty FolderCategoryProperty =
+            DependencyProperty.Register(
+               name: "FolderCategory",
+               propertyType: typeof(string),
+               ownerType: typeof(FolderControl),
+               typeMetadata: new FrameworkPropertyMetadata(
+                   defaultValue: "none",
+                   flags: FrameworkPropertyMetadataOptions.AffectsMeasure),
+               validateValueCallback: new ValidateValueCallback(IsValidString));
+
+        public string FolderCategory
+        {
+            get => (string)GetValue(FolderCategoryProperty);
+            set => SetValue(FolderCategoryProperty, value);
+        }
+
+        // folder cards count
+        public static readonly DependencyProperty FolderCardsCountProperty =
+            DependencyProperty.Register(
+               name: "FolderCardsCount",
+               propertyType: typeof(int),
+               ownerType: typeof(FolderControl),
+               typeMetadata: new FrameworkPropertyMetadata(
+                   defaultValue: 0,
+                   flags: FrameworkPropertyMetadataOptions.AffectsMeasure),
+               validateValueCallback: new ValidateValueCallback(IsValidCardsCount));
+
+        public int FolderCardsCount
+        {
+            get => (int)GetValue(FolderCardsCountProperty);
+            set => SetValue(FolderCardsCountProperty, value);
+        }
+
+        // folder memorized cards count
+        public static readonly DependencyProperty FolderMemorizedCardsCountProperty =
+            DependencyProperty.Register(
+               name: "FolderMemorizedCardsCount",
+               propertyType: typeof(int),
+               ownerType: typeof(FolderControl),
+               typeMetadata: new FrameworkPropertyMetadata(
+                   defaultValue: 0,
+                   flags: FrameworkPropertyMetadataOptions.AffectsMeasure),
+               validateValueCallback: new ValidateValueCallback(IsValidCardsCount));
+
+        public int FolderMemorizedCardsCount
+        {
+            get => (int)GetValue(FolderMemorizedCardsCountProperty);
+            set => SetValue(FolderMemorizedCardsCountProperty, value);
+        }
+
+        public static bool IsValidCardsCount(object value)
+        {
+            int val = (int)value;
+            return val >= 0;
+        }
     }
 }
